4be9a23d21d2d83daf34a14d22198eacee367ee2
Karl Prieb <karlprieb@Karls-MacBook-Pro.local>
2015-06-28 19:56:58 -0300

    Test works

--- woff2.rb
+++ woff2.rb
@@ -2,17 +2,20 @@ class Woff2 < Formula
   desc "Woff2 compress and decompress by Google"
   homepage "https://github.com/prieb/woff2-homebrew"
   url "https://github.com/prieb/woff2-homebrew.git"
-  version "0.1"
+  version "0.2"
 
   def install
-    system "cd woff2"
     system "git submodule init"
     system "git submodule update"
-    system "make clean all"
-    bin.install "woff2_compress", "woff2_decompress"
+    cd "woff2" do
+      system "git submodule update"
+      system "make clean all"
+      bin.install "woff2_compress", "woff2_decompress"
+    end
+    share.install "tests/OpenSans-Regular.ttf"
   end
 
   test do
-    system "#{bin}/woff2_compress tests/OpenSans-Regular.ttf"
+    system "#{bin}/woff2_compress #{share}/OpenSans-Regular.ttf"
   end
 end
